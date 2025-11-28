88019f96f50f524d6fa2a88350079ec5eafbeb37
Vane1013 <veref777@gmail.com>
2025-11-28 16:48:17 +0300

    purchase_form.js

--- purchase_form.js
+++ purchase_form.js
@@ -4,7 +4,7 @@ const managers = {
         name: 'Vanya',
         email: 'vanya@football.com',
         license: 'FIFA-AGENT-001',
-        budget: 5,
+        budget: 0,
         welcomeShown: false  // Флаг для отслеживания показа приветствия
     },
     'Serega': {
@@ -249,6 +249,7 @@ function closeSuccessMessage() {
     document.getElementById('successMessage').style.display = 'none';
     window.location.href = 'all_players.html';
 }
+
 
 
 
